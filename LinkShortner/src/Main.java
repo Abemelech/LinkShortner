6378c2d8b55afc051c8b13c4c59140c814a1c9dd
Abemelech <abemelech.mesfin@gmail.com>
2022-01-17 21:37:43 -0600

    comment everything

--- LinkShortner/src/Main.java
+++ LinkShortner/src/Main.java
@@ -14,30 +14,35 @@ import org.apache.commons.validator.routines.UrlValidator;
 public class Main {
     public static void main (String[] args) throws MalformedURLException, IOException, URISyntaxException {
 
+        // A hashmap to store the link and the shortned link
         HashMap<String, String> linkBank = new HashMap<String, String>();
 
         linkBank.put("AM.com/google", "www.google.com");
 
+        // Console prints of instruction for the user to choose between creating a link or getting redirected
         System.out.println("Welcome to AM's Shortner. WE DO LIKE THEM SHORT!!!");
         System.out.println("What would you like to choose?");
         System.out.println("1. Create a short link");
         System.out.println("2. Get re-directed to a website");
         System.out.println("Enter only a number to decide.");
 
+        // A Scanner for user inputs on the decision
         Scanner userInput = new Scanner(System.in);
         char menuDecision = userInput.next().charAt(0);
 
+        // If the decision is to make a shortned link then initialize ShortLink class
         if (menuDecision == '1'){
-            //Run another class
+            //Get link and shortned link and store them in the link bank
             ShortLink start = new ShortLink();
             linkBank.put(start.getShort(), start.getlink());
             System.out.println(linkBank);
 
         } else if (menuDecision == '2') {
-            //Run another class
+            //If the decision was to redirect then initialize Redirect class
             Redirect menu2 = new Redirect();
             String website = linkBank.get(menu2.getUrl());
 
+            // Get url from user and get access of the long url and head to a website 
             System.out.println(website);
             Desktop desktop = Desktop.getDesktop();
 
@@ -54,9 +59,12 @@ public class Main {
 class ShortLink {
     String link;
     ShortLink(){
+        // Get user input of the link they want to shorten
         System.out.println("What link do you want to shorten?");
         Scanner userInput = new Scanner(System.in);
         this.link = userInput.nextLine();
+
+        // Validate if the url is valid and store link
         String[] schemes = {"http","https"};
         UrlValidator urlValidator = new UrlValidator(schemes);
 
@@ -69,14 +77,18 @@ class ShortLink {
 
     } 
     public String getlink() {
+        // Getter for the link
         return this.link;
     }
 
     public String getShort() {
+        // Ask if the user want a custom short url
         System.out.println("Do you want to create a custom url. If 'yes', write down the word. If 'no', type 'no'");
         Scanner userInput = new Scanner(System.in);
         String specificInput = userInput.nextLine().toString();
         String shortlinkEnd;
+
+        // Run a randomizer and generate a 6 letter string
         if (specificInput.equals("no")) {
             Randomizer shorter = new Randomizer(6);
             shortlinkEnd = shorter.nextString();
@@ -95,12 +107,14 @@ class ShortLink {
 class Redirect {
     String url;
     Redirect(){
+        // Get user input of what link the user wants to access
         System.out.println("Enter your shortned URL to get redirected to the official website");
         Scanner userInput = new Scanner(System.in);
         this.url = userInput.nextLine().toString();       
     }
 
     public String getUrl() {
+        // Send url
         return this.url;
     }
 }
